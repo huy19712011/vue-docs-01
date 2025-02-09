35cbd95c89d7a47f2c9e6b586740fe10b82b7ea8
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-02-09 23:32:02 +0700

    2. Attribute Binding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { reactive, ref } from "vue";
 
+const titleClass = ref("title");
+
+/* 
+//1. ref and reactive
 const counter = reactive({ count: 0 });
 console.log(counter.count);
 counter.count++;
@@ -8,9 +12,20 @@ counter.count++;
 const message = ref("Hello World");
 console.log(message.value); // Hello World
 message.value = "Changed!";
+*/
 </script>
 
 <template>
+  <h1 :class="titleClass">Make me red</h1>
+
+  <!-- 1
   <h1>{{ message }}</h1>
   <p>Count is: {{ counter.count }}</p>
+ -->
 </template>
+
+<style scoped>
+.title {
+  color: red;
+}
+</style>
