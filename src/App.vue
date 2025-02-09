f5df81480a7d5315c3eb8d1d8727549e83912183
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-02-09 23:18:13 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { reactive, ref } from "vue";
+
+const counter = reactive({ count: 0 });
+console.log(counter.count);
+counter.count++;
+
+const message = ref("Hello World");
+console.log(message.value); // Hello World
+message.value = "Changed!";
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <p>Count is: {{ counter.count }}</p>
+</template>
